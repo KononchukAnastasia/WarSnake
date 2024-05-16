a58fc38f0324c75c5b67f3af45432939411f092a
Anastasia Kononchuk <nastyakononchuk@me.com>
2024-05-16 20:09:06 +0700

    Animated snake crawling.

--- WarSnake/PlayerSnake.swift
+++ WarSnake/PlayerSnake.swift
@@ -12,6 +12,7 @@ class PlayerSnake: SKSpriteNode {
     let motionManager = CMMotionManager()
     var xAcceleration: CGFloat = 0
     let screenSize = CGSize(width: UIWindow.bounds.width, height: UIWindow.bounds.height)
+    var moveTextureArrayAnimation = [SKTexture]()
     
     static func populate(at point: CGPoint) -> PlayerSnake {
         let playerSnakeTexture = SKTexture(imageNamed: "snake4")
@@ -33,12 +34,43 @@ class PlayerSnake: SKSpriteNode {
     }
     
     func performСrawl() {
+        playingAnimationFillArray {
+            let snakeAnimationAction = SKAction.animate(
+                with: self.moveTextureArrayAnimation,
+                timePerFrame: 0.1,
+                resize: true,
+                restore: false
+            )
+            
+            let snakeAnimationForever = SKAction.repeatForever(snakeAnimationAction)
+            self.run(snakeAnimationForever)
+        }
+        
         motionManager.accelerometerUpdateInterval = 0.2
-        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { data, error in
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { [unowned self] (data, error) in
             if let data = data {
                 let acceleration = data.acceleration
                 self.xAcceleration = acceleration.x * 0.7 + self.xAcceleration * 0.3
             }
+        }
+    }
+    
+    fileprivate func playingAnimationFillArray(completion: @escaping () -> Void) {
+        SKTextureAtlas.preloadTextureAtlases([SKTextureAtlas(named: "playerSnake")]) { [unowned self] in
+            self.moveTextureArrayAnimation = {
+                var array = [SKTexture]()
+                for i in stride(from: 1, through: 8, by: 1) {
+                    let number = String(format: "%d", i)
+                    let texture = SKTexture(imageNamed: "snake\(number)")
+                    array.append(texture)
+                }
+                
+                SKTexture.preload(array) {
+                    print("preload is done")
+                    completion()
+                }
+                return array
+            }()
         }
     }
 }
